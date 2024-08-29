acbb82db4d7d1b759609e5a220fcfa2b9bfcae87
Martin Dostál <martin.dostal@redbit.cz>
2024-08-29 15:28:20 +0200

    Martin Dostál - DU 5

--- lesson_5/Robot.py
+++ lesson_5/Robot.py
@@ -1,20 +1,63 @@
-import Svetlo
+from neopixel import NeoPixel
+from microbit import pin0
+from microbit import sleep
 
 class Robot:
-    def __init__():
+    def __init__(self):
+        self.np = NeoPixel(pin0, 8)
+        self.COLOR_RED = (60, 0, 0)
+        self.COLOR_RED_MAX = (255, 0, 0)
+        self.COLOR_WHITE = (255, 255, 255)
+        self.COLOR_YELLOW = (247, 247, 1)
+        self.COLOR_BLACK = (0, 0, 0)
 
-    def indikuj(smer_zataceni):
-        #pro ted je smer_zataceni string "doprava" nebo "doleva"
-        #TODO - blikejte s vnejsimi ledkami oranzovou barvou, zvolte periodu blikani
+        self.vypni_svetla()
 
-    def zapni_svetla():
-        #TODO zapnete predni svetla (vnitrni LED) a zadni svetla - zda doporucuji barvu (60,0,0)
+    def indikuj(self, smer_zataceni):
+        led1 = None
+        led2 = None
+        if smer_zataceni == "doprava":
+            led1 = 1
+            led2 = 7
+        elif smer_zataceni == "doleva":
+            led1 = 3
+            led2 = 4
+        if(led1 is not None):
+            for i in range(0,3):
+                self.np[led1] = self.COLOR_YELLOW
+                self.np[led2] = self.COLOR_YELLOW
+                self.np.write()
+                sleep(500)
+                self.np[led1] = self.COLOR_BLACK
+                self.np[led2] = self.COLOR_BLACK
+                self.np.write()
+                sleep(500)
 
-    def brzdi():
-        #TODO zadni svetla by mela svitit barvou (255,0,0)
+    def zapni_svetla(self):
+        self.np[1] = self.COLOR_WHITE
+        self.np[2] = self.COLOR_WHITE
+        self.np[5] = self.COLOR_RED
+        self.np[6] = self.COLOR_RED
+        self.np.write()
 
-    def vypni_svetla():
-        #TODO
 
+    def brzdi(self):
+        self.np[5] = self.COLOR_RED_MAX
+        self.np[6] = self.COLOR_RED_MAX
+        self.np.write()
 
+    def vypni_svetla(self):
+        for i in range(0, 8):
+            self.np[i] = self.COLOR_BLACK
+        self.np.write()
 
+robot = Robot()
+robot.zapni_svetla()
+sleep(1000)
+robot.brzdi()
+sleep(1000)
+robot.vypni_svetla()
+sleep(1000)
+robot.indikuj("doprava")
+sleep(1000)
+robot.indikuj("doleva")
