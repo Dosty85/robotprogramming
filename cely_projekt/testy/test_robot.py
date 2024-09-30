bb5db7b2211dfd3018972f65bfbd9408458cdd55
Martin Dostál <martin.dostal@redbit.cz>
2024-09-30 14:29:05 +0200

    Kalibrace motoru

--- cely_projekt/testy/test_robot.py
+++ cely_projekt/testy/test_robot.py
@@ -4,19 +4,19 @@ from cely_projekt import Konstanty, KalibracniFaktory, Robot
 
 def zakladni_test_spusteni():
 
-    min_rychlost = 2.1658lu
-    min_pwm_rozjezd = 79 # kalibrace vytiskne na konci pri "zrychluj"
-    min_pwm_dojezd = 41 # kalibrace vytiskne na konci pri "zpomaluj"
-    a = 24.3732783404646 # ziskej z excelu
-    b = 8.21172006498485 # ziskej z excelu
+    min_rychlost = 0.6233437
+    min_pwm_rozjezd = 57 # kalibrace vytiskne na konci pri "zrychluj"
+    min_pwm_dojezd = 54 # kalibrace vytiskne na konci pri "zpomaluj"
+    a = 25.4585241095727 # ziskej z excelu
+    b = 41.1305893849997 # ziskej z excelu
 
     levy_faktor = KalibracniFaktory(min_rychlost, min_pwm_rozjezd, min_pwm_dojezd, a, b)
 
-    min_rychlost = 2.171571
-    min_pwm_rozjezd = 113
-    min_pwm_dojezd = 113
-    a = 27.4515630414309
-    b = 61.3869817945568
+    min_rychlost = 0.7791749
+    min_pwm_rozjezd = 57
+    min_pwm_dojezd = 54
+    a = 25.4604475451773
+    b = 39.1618583300312
 
     pravy_faktor = KalibracniFaktory(min_rychlost, min_pwm_rozjezd, min_pwm_dojezd, a, b)
 
